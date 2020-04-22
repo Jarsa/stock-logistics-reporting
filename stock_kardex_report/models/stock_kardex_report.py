79871e186fbd41a4898cd90df658817748715c66
tino480 <2tino480@gmail.com>
2020-04-22 07:00:04 -0500

    Ran pre-commit

--- stock_kardex_report/models/stock_kardex_report.py
+++ stock_kardex_report/models/stock_kardex_report.py
@@ -5,19 +5,19 @@ from odoo import fields, models
 
 
 class StockKardexReport(models.Model):
-    _name = 'stock.kardex.report'
-    _description = 'This model creates a kardex report for stock moves'
-    _order = 'date desc'
+    _name = "stock.kardex.report"
+    _description = "This model creates a kardex report for stock moves"
+    _order = "date desc"
 
-    move_id = fields.Many2one('stock.move', readonly=True)
-    product_id = fields.Many2one('product.product', readonly=True)
-    product_uom_id = fields.Many2one('uom.uom', readonly=True)
-    lot_id = fields.Many2one('stock.production.lot', readonly=True)
-    owner_id = fields.Many2one('res.partner', readonly=True)
-    package_id = fields.Many2one('stock.quant.package', readonly=True)
-    location_id = fields.Many2one('stock.location', readonly=True)
-    location_dest_id = fields.Many2one('stock.location', readonly=True)
-    qty_done = fields.Float('Done', readonly=True)
+    move_id = fields.Many2one("stock.move", readonly=True)
+    product_id = fields.Many2one("product.product", readonly=True)
+    product_uom_id = fields.Many2one("uom.uom", readonly=True)
+    lot_id = fields.Many2one("stock.production.lot", readonly=True)
+    owner_id = fields.Many2one("res.partner", readonly=True)
+    package_id = fields.Many2one("stock.quant.package", readonly=True)
+    location_id = fields.Many2one("stock.location", readonly=True)
+    location_dest_id = fields.Many2one("stock.location", readonly=True)
+    qty_done = fields.Float("Done", readonly=True)
     date = fields.Datetime(readonly=True)
     origin = fields.Char(readonly=True)
     balance = fields.Float(readonly=True)
